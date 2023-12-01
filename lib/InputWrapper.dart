4f3423409da4a2593e9f043fdb51cf246f31738d
Gagan Deep Kashyap <cu.gagankashyap@gmail.com>
2023-12-01 10:45:46 +0530

    today changes

---
+++ lib/InputWrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class InputWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: EdgeInsets.all(30),
+    child: Column(),)
+  }
+}
