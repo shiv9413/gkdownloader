902632c41a16c1c184455ecad9b0f0f39b6e9239
gagankashyap <cu.gagankashyap@gmail.com>
2024-01-03 12:25:46 +0530

    ch

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
 
     return new MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: HomePage(),
     );
   }
